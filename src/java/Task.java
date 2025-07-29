4f7f726725357326d97a060e0c1cbaabf3c35f28
real-deepmind <real.deepmind@gmail.com>
2025-07-29 12:48:22 +0330

    update Task Class--add constructor and getter and setter method

--- src/java/Task.java
+++ src/java/Task.java
@@ -1,4 +1,43 @@
 public class Task {
     private String description;
-    private String timeOffer;
+    private int timeOffer;
+    public Task(String description, int timeOffer) throws InvalidTaskException {
+        if (description == null || description.trim().isEmpty()) {
+            throw new InvalidTaskException("Description cannot be null or empty");
+        }
+        if (timeOffer <= 0) {
+            throw new InvalidTaskException("Time offer cannot be negative");
+        }
+        this.description = description;
+        this.timeOffer = timeOffer;
+    }
+    //get time
+    public String getDescription() {
+        return description;
+    }
+    //set time
+    public void setDescription(String description) throws InvalidTaskException  {
+        if (description == null || description.trim().isEmpty()) {
+            throw new InvalidTaskException("Description cannot be null or empty");
+        }
+        this.description = description;
+
+    }
+    //get time
+    public int getTimeOffer() {
+        return timeOffer;
+    }
+    //set time
+    public void setTimeOffer(int timeOffer) throws InvalidTaskException {
+        if (timeOffer <= 0) {
+            throw new InvalidTaskException("Time offer cannot be negative");
+        }
+        this.timeOffer = timeOffer;
+    }
+    @Override
+    public String toString() {
+        return "task: " + description + ", time offer: " + timeOffer;
+    }
+
+
 }
