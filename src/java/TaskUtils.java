903e0a7e1e488d4b382cef9c714db679f25ea3c5
real-deepmind <real.deepmind@gmail.com>
2025-07-29 13:59:30 +0330

    create //filter tasks and return list of filtered tasks  class

---
+++ src/java/TaskUtils.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+//filter tasks and return list of filtered tasks
+public class TaskUtils {
+    public List<Task> filterLongTasks(List<Task> tasks , Predicate<Task> predicate) {
+        List<Task> result = new ArrayList<>();
+        for (Task task : tasks) {
+            if (predicate.test(task)) {
+                result.add(task);
+            }
+        }
+        return result;
+    }
+}
