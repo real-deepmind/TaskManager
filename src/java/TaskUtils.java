43a8708dff1279b883e8a26c78180b55902dec31
real-deepmind <real.deepmind@gmail.com>
2025-07-29 14:31:34 +0330

    method mapTasksToStrings -- use Function<Task, String> mapper method processTasks --use Consumer<Task> action

--- src/java/TaskUtils.java
+++ src/java/TaskUtils.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
 
 //filter tasks and return list of filtered tasks
@@ -12,5 +14,21 @@ public class TaskUtils {
             }
         }
         return result;
+    }
+    //
+
+    public static List<String> mapTasksToStrings(List<Task> tasks, Function<Task, String> mapper) {
+        List<String> result = new ArrayList<>();
+        for (Task task : tasks) {
+            String mappedItem = mapper.apply(task);
+            result.add(mappedItem);
+        }
+        return result;
+    }
+    //
+    public void processTasks(List<Task> tasks, Consumer<Task> action) {
+        for (Task task : tasks) {
+            action.accept(task);
+        }
     }
 }
